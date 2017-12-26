b8793501ed5b375c192156c04dc73e409a01dca8
BukinPK <bukinpk@gmqil.com>
2017-12-26 19:00:58 +0200

    Небольшое структурирование и минорные изменения

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -3,28 +3,27 @@ from datetime import datetime
 import json
 
 class Block:
-	def __init__(this, index, prevHash, timestamp, level, nonce, data, Ahash):
-		this.index = index
-		this.prevHash = prevHash
-		this.timestamp = timestamp
-		this.level = level
-		this.nonce = nonce
-		this.data = data #Добавить вознаграждение
-		this.hash = Ahash
+	def __init__(self, index, prevHash, timestamp, level, nonce, data, Ahash):
+		self.index = index
+		self.prevHash = prevHash
+		self.timestamp = timestamp
+		self.level = level
+		self.nonce = nonce
+		self.data = data #Добавить вознаграждение
+		self.hash = Ahash
 	def parse(self):
 		return [self.index, self.prevHash, self.timestamp, self.level, self.nonce, self.data, self.hash]
 
 genesisBlock = Block(0,'0',1513693852,1,0,'This is the Genesis Block!', 
 	'9529788d61b65be3d1796a04a348a46f5b30e6bc28fc293b29f7277ab48a7be0')
 blockchain = [genesisBlock]
-def getLastBlock(): return blockchain[len(blockchain) - 1]
 
 def calcHash(index, prevHash, timestamp, level, nonce, data): return sha256(str(index).encode() + prevHash.encode() + str(timestamp).encode() + str(level).encode() + str(nonce).encode() + data.encode()).hexdigest()
 def calcHashBlock(block):
 	return calcHash(block.index, block.prevHash, block.timestamp, block.level, block.nonce, block.data)
 
 def genNextBlock(blockData): 
-	prevBlock = getLastBlock()
+	prevBlock = blockchain[-1]
 	nextIndex = prevBlock.index + 1
 	nextTimestamp = int(datetime.today().timestamp())
 	nextLevel = 3
@@ -57,14 +56,19 @@ def checkValidBlock(prevBlock, newBlock):
 	return True
 
 def addBlock(newBlock):
-	if checkValidBlock(getLastBlock(), newBlock):
+	if checkValidBlock(blockchain[-1], newBlock):
 		blockchain.append(newBlock)
 
-#Сделать проверку цепочки (длинная в приоритете, дубликаты отбрасываются)
 def checkValidChain(newBlockchain):
+	"""
+	Проверяет цепочку на валидность, 
+	в случае успешной проверки, возвращает часть цепочки, 
+	которую стоит добавить к актуальной 
+	(длинная в приоритете, дубликаты отбрасываются)
+	"""
 	goodPart = []
 	for block in newBlockchain[::-1]:
-		if block.index > getLastBlock().index or block.prevHash != blockchain[block.index-1].hash and block.index != 0:
+		if block.index > blockchain[-1].index or block.prevHash != blockchain[block.index-1].hash and block.index != 0:
 			if checkValidBlock(newBlockchain[(newBlockchain.index(block))-1], block):
 				goodPart.append(block)
 				continue
@@ -73,27 +77,29 @@ def checkValidChain(newBlockchain):
 		else: return False
 
 def replaceChain(newBlockchain):
-	if len(newBlockchain)-1 > getLastBlock().index:
+	if len(newBlockchain)-1 > blockchain[-1].index:
 		goodPart = checkValidChain(newBlockchain)
 		if goodPart:		
-			for index in range(getLastBlock().index - goodPart[0].index):
+			for index in range(blockchain[-1].index - goodPart[0].index):
 				del blockchain[-1]
 			blockchain.extend(goodPart)
 			print('Цепочка успешно импортирована')
 		else: print('Цепочка не валидна')
 		#Рассказать всем	
-	else: print("Цепочка коротче, либо равна актуальной на %s блоков" % (getLastBlock().index - (len(newBlockchain)-1)))
+	else: print("Цепочка коротче, либо равна актуальной на %s блоков" % (blockchain[-1].index - (len(newBlockchain)-1)))
 
+#Сохранение и загрузка цепочки
 def chainExport(saved_chain='saved_chain.json'):
-	with open(saved_chain, 'w') as fd:
+	with open(saved_chain, 'w') as f:
 		parsedBlockchain = []
 		for block in blockchain:
 			parsedBlockchain.append(block.parse())
-		fd.write(json.dumps(parsedBlockchain))
+		f.write(json.dumps(parsedBlockchain))
+		print('Цепочка успешно экспортирована в файл %s' % saved_chain)
 
 def chainImport(saved_chain='saved_chain.json'):
-	with open(saved_chain, 'r') as fd:
-		parsedBlockchain = json.loads(fd.read())
+	with open(saved_chain, 'r') as f:
+		parsedBlockchain = json.loads(f.read())
 	newBlockchain = []
 	for block in parsedBlockchain:
 		newBlockchain.append(Block(*block))

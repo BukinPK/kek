5cddf669f4320faed16322adb3ccc74e6831bcda
BukinPK <bukinpk@gmqil.com>
2018-01-14 17:06:34 +0200

    neuro3

--- neuro/neuro.py
+++ neuro/neuro.py
@@ -3,18 +3,16 @@ from img import load_image, save_image
 
 class Neuron:
 	weight = 0
-	def __init__(self, name, input=[0 for x in range(30*30)], model=[255 for x in range(30*30)], count=0):
+	def __init__(self, name, model=[255 for x in range(30*30)], count=0):
 		self.name = name
-		self.input = input
 		self.model = model
 		self.count = count
 	def __repr__(self):
 		return 'Нейрон:%s Уверенность:%i Модель:%i' % (self.name, self.weight, self.count)
-	@property
-	def add(self):
+	def add(self, input):
 		temp = []
 		for num, pixel in enumerate(self.model):
-			temp.append(int((pixel * self.count + self.input[num]) / (self.count + 1)))
+			temp.append(int((pixel * self.count + input[num]) / (self.count + 1)))
 		self.model = temp
 		self.count += 1
 	def save(self, path='pic/model/'):
@@ -32,6 +30,7 @@ class Neuron:
 class Web:
 	__view = []
 	match = None
+	_input = [0 for x in range(30*30)]
 	def __init__(self, elements=string.ascii_uppercase):
 		for item in elements:
 			self.__view.append(Neuron(item))
@@ -39,31 +38,26 @@ class Web:
 		return repr(self.__view)
 	def __getitem__(self, key):
 		return self.__view[key]
-
 	def insert(self, pic):
-		for neuron in self: neuron.input = load_image(pic)
-
+		self._input = load_image(pic)
 	def check(self):
 		for neuron in self:
+			neuron.weight = 0
 			for pixel in range(900):
-				if neuron.input[pixel] < 250 and abs(neuron.model[pixel] - neuron.input[pixel]) < 100:
+				if self._input[pixel] < 250 and abs(neuron.model[pixel] - self._input[pixel]) < 100:
 					neuron.weight += 1	
 		max = self[0]
 		for neuron in self:
 			if neuron.weight > max.weight: max = neuron
 		self.match = max
 		return max
-
 	def result(self):
-		for neuron in self:
-			print(neuron)
-
+		for neuron in self: print(neuron)
 	def learn(self, path='pic/input/'):
 		for pic in os.listdir(path):
 			for neuron in self:
 				if neuron.name == pic[0].upper():
-					neuron.input = load_image(path+pic)
-					neuron.add
+					neuron.add(load_image(path+pic))
 	def save(self, path='pic/model/'):
 		for neuron in self:
 			if neuron.count > 0:

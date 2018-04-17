57a035968d771a470b62c26e97eb9a8f96869c37
BukinPK <bukinpk@gmqil.com>
2018-04-18 01:47:34 +0300

    random_rate add

---
+++ random_rate.py
@@ -0,0 +1,33 @@
+from random import randint as rand
+from sys import argv
+
+def repeat_count(try_count, debug=True):
+	global_storage = {}
+	storage = 0
+	last_try = None
+	while try_count > 0:	
+		try_count -= 1
+		this_try = rand(0,1)
+
+		if this_try is not last_try and last_try is not None or try_count == 0:
+			if this_try == last_try:
+				storage += 1
+			if debug: print(str(last_try)*storage)
+			if try_count == 0 and this_try != last_try:
+				if debug: print(this_try, 'kek')
+				try: global_storage[1] += 1
+				except KeyError: global_storage[1] = 1
+			try:
+				global_storage[storage] += 1
+			except KeyError: 
+				global_storage[storage] = 1
+			storage = 1
+		else:
+			storage += 1
+		last_try = this_try
+
+	return global_storage
+
+storage = repeat_count(int(argv[1]))
+for key in sorted(storage.keys()):
+	print('%-3i подряд выпало %i раз' % (key, storage[key]))

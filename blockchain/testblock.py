3b8b5137f398b64692a47017113e6c9a6058a5b4
BukinPK <bukinpk@gmqil.com>
2018-06-17 20:22:28 +0300

    blockchain del

--- blockchain/testblock.py
+++
@@ -1,8 +0,0 @@
-from blockchain import *
-addBlock(findNonce(genNextBlock('lel')))
-addBlock(findNonce(genNextBlock('lel1')))
-addBlock(findNonce(genNextBlock('lel2')))
-addBlock(findNonce(genNextBlock('lel3')))
-addBlock(findNonce(genNextBlock('lel4')))
-addBlock(findNonce(genNextBlock('lel5')))
-chainExport()
